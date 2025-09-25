50b62eb1ca5e51d010712b0f602fab3f2f87c54c
Alessandro <jamancapiero85@gmail.com>
2025-09-24 23:51:49 -0300

    Ending of Service Entiti

---
+++ Domain/Repositories/IServiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories;
+public interface IServiceRepository
+{
+        Task<IEnumerable<Service>> GetAllServicesAsync();
+        Task<Service?> GetServiceByIdAsync(int id);
+        Task<Service> AddServiceAsync(Service service);
+        Task UpdateServiceAsync(Service service);
+        Task DeleteServiceAsync(int id);
+}
+}
+
