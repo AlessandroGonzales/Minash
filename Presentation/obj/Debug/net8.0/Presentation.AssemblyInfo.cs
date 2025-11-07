62bb63fb1cbe062a6747c529b0db74342d6b7423
Alessandro <jamancapiero85@gmail.com>
2025-11-07 20:27:13 -0300

    Hotfix on production

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d0bbbbd76ffbdb770f88472082ad02097c8145")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ed1e9e6f94f94d607a983173de1e70dd5f22b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
