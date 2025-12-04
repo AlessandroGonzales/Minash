eda16baa7eded9f7d6fc35f21bbafe747a9cfe6a
Alessandro <jamancapiero85@gmail.com>
2025-12-03 23:24:50 -0300

    Add cors and logic

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbfde1c6b846f193da5385a8fde4a6e805bda312")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb4a0968831ea20b011e3754115798c0473d942f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
